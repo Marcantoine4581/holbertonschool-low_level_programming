019b60b61f92373986b5a9d2cc508e543d42ae1f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-03 04:44:41 -0700

    ex 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		printf("%c ", ch);
+		ch++;
+	}
+	printf("\n");
+	return (0);
+}
