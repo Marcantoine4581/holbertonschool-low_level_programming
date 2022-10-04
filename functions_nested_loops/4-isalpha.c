430f4f92171eea4e3ff8cb2046a390b906afdc67
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-04 10:16:23 -0700

    ex 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * isalpha - checks  for  an  alphabetic  character; in the standard "C" locale
- * it is equivalent to (isupper(c) || islower(c)).
- * In some locales, there may be  additional characters for which
+ * _isalpha - checks for an alphabetic character; in the standard "C"
+ * locale it is equivalent to (isupper(c) || islower(c)).
+ * In some locales, there may be additional characters for which
  * isalpha() is true—letters which are neither uppercase nor lowercase.
  * @c: the character to check
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
