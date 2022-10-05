d59f69b60c86139f66f0477773583906a989f9f0
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-05 01:33:48 -0700

    ex 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ * @x: number being added
+ * @y: number being added
+ * Return: the result of the addition
+ */
+int add(int x, int y)
+{
+	_putchar(x + y);
+}
