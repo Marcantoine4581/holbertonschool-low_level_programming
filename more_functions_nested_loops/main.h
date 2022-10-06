0cbc81a437c1a63e699c8a3d7ec9f3dbc73ee389
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-06 00:26:06 -0700

    add files

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef main
+#define main
+
+/**
+ * File: main.h
+ * Author: Marc-Antoine VANNIER
+ * Description: Header file containing prototypes for all functions
+ * used for more_functions_nested_loops directory.
+ */
+
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
