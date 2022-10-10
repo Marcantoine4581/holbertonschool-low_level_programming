8c2db01f204cbbc9f95c96ee6a26a6166918bda8
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-10 02:58:49 -0700

    ex 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square, followed by a new line.
+ * @size: is the size of the square
+ * If size is 0 or less, the function should print only a new line
+ * Use the character # to print the square
+ * Return:
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
