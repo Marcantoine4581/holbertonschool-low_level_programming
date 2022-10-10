7c2363763239f8fa4acde692eb2f1b15896678e8
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-10 02:21:09 -0700

    first commit

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * File: main.h
+ * Author: Marc-Antoine VANNIER
+ * Description: Header file containing prototypes for all functions
+ * used for pointers_arrays_strings directory.
+ */
+
+int _putchar(char);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
