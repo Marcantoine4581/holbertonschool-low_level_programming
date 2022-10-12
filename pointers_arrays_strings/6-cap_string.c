4699158ccd547b3db062b67c57516df715ff99bb
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-12 08:27:22 -0700

    ex 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words of a string
+ *
+ * @str: The string to be changed.
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char spec_char[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+			for (j = 0; spec_char[j] != '\0'; j++)
+				if (str[i] == spec_char[j] && str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+	}
+	return (str);
+}
