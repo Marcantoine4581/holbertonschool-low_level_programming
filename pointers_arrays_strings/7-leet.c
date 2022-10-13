5698ce57884ca87cbe542415523d9b6adf1762b8
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-13 01:04:27 -0700

    ex 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337
+ *
+ * @str: The string to be changed.
+ * Return: string that is encoded.
+ */
+char *leet(char *str)
+{
+	char *strenc = str;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {'4', '3', '0', '7', '1'};
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*str == key[i] || *str == key[i] + 32)
+			{
+				*str = '0' + value[i];
+			}
+		}
+	str++
+	}
+
+	return (strenc);
+}
