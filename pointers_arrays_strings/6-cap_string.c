0684887f65cb9736257e8bfc06c5674e04960728
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-13 00:22:05 -0700

    ex 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[0] >= 'a' && str[0] <= 'z')
 			str[i] -= 32;
 				for (j = 0; spec_char[j] != '\0'; j++)
 					if (str[i] == spec_char[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
