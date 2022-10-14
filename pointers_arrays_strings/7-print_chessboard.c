74fe05a5e32e15494d1395ba642119a5922a7d5b
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-14 06:28:05 -0700

    ex 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: The chessboard to be printed.
+ * Return:
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	x = 0;
+	while (x < 8)
+	{
+		y = 0;
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
