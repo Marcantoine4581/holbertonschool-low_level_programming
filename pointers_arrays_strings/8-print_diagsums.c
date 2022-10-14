82c1e52ff919386540388f328e3e05b70e2cee6e
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-14 09:09:48 -0700

    ex 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0, i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i];/*sum1 = sum1 + value of a[i]*/
 		a += size;/*a = a + size (example : size 3 = 4 because 0 1 2 3 */
