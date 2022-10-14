453a0d5b98c9a2d787f8f98909a117ef443aed9d
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-14 01:59:40 -0700

    ex 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,18 +9,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
-	int bytes;
+	int bytes = 0;
 
 	while (*s)
 	{
-		for (i = 0; accept[i] ; i++)
+		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 			{
 				bytes++;
 				break;
 			}
-			
+
 			else if (accept[i + 1] == '\0')
 				return (bytes);
 		}
