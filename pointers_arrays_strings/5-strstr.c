9dd476da1ac9e8f073c59b0358abc6b081e38fa4
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-14 06:15:34 -0700

    ex 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
