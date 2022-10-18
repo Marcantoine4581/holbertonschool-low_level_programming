f7b81ec3087faff2769b511969415d0936de2d6a
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-18 00:26:33 -0700

    ex 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -24,11 +24,11 @@ int is_prime_number(int n)
  */
 int isPrime(int n, int i)
 {
-	if (n % i == 0)
-		return (0);
-
 	if (i == 1)
 		return (1);
+
+	if (n % i == 0)
+		return (0);
 
 	return (isPrime(n, i - 1));
 }
