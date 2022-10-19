81125309c7b6bd3349e8fbc3ec19a4358ac93ab4
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-19 07:06:55 -0700

    ex 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 1 if error, 0 if function runs correctly
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+
+	else
+		printf("ERROR\n");
+
+	return (1);
+}
