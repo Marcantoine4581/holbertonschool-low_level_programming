a4c061775ec7761f06aa5b1d02a988a8d0031a30
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-19 00:29:03 -0700

    putchar

--- static_libraries/_putchar.c
+++ static_libraries/_putchar.c
@@ -1,18 +1,5 @@
 #include "main.h"
-/**
-  * main - Entry point
-  * write a program tha prints _putchar, followed by a new line.
-  * Return: Always 0 (Success)
-  */
-int main(void)
-{
-	char *ch = "_putchar";
 
-	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
-	_putchar('\n');
-	return (0);
+int _putchar(char c)
+{
 }
