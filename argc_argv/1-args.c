803b73ff6c16cf92e68d7221b0f0a0caa7748606
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-19 05:24:12 -0700

    ex 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
