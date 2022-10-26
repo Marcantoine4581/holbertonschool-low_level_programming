cb98e332c581e71814c68912de18dcb0d78184ba
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-26 06:39:45 -0700

    ex 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,16 +19,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[size1])
+	while (s1[size1] != '\0')
 		size1++;
 
-	while (s2[size2])
+	while (s2[size2] != '\0')
 		size2++;
 
 	if (n > size2)
 		n = size2;
 
-	concat = malloc(sizeof(char) * (size1 + n));
+	concat = malloc(sizeof(char) * (size1 + n + 1));
 
 	if (concat == NULL)
 		return (NULL);
@@ -38,6 +38,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (; i < (size1 + n); i++)
 		concat[i] = s2[i - size1];
+
+	concat[i] = '\0';
 
 	return (concat);
 }
