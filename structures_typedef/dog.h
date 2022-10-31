727d57900fc4eb43f180f022c9c85a14f228d9f7
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-10-31 02:58:06 -0700

    ex 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Header file containing the type describing a dog.
+ * @name : pointer for the name of the dog.
+ * @age: age of the dog.
+ * @owner: pointer for the owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
