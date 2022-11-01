db07a881581ab358378b271dd029fe594cac02e8
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-01 04:33:30 -0700

    ex 0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -12,7 +12,6 @@ int _putchar(char);
  * print_name - Print a name from a function pointer.
  * @name: The name to be printed.
  * @f: function pointer that takes a string argument.
- * @char: string argument.
  */
 void print_name(char *name, void (*f)(char *));
 
