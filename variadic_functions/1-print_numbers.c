e50a7fa7a90631aed34366665c63025b32351204
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-02 08:45:24 -0700

    ex 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,26 +1,30 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_number - function that prints numbers, followed by a new line.
- * @n: number of arguements.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
  * @...: A variable number of parameters to calculate the sum of.
- * Return: 
+ * If separator is NULL, don’t print it.
+ * Return: nothing.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list num;
 	unsigned int i;
-	int sum;
 
 	va_start(num, n);
-	
+
 	for (i = 0; i < count; i++)
-		sum += va_arg(num, int);
+	{
+		printf("%d", va_arg(num, int);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
 
 	va_end(num);
-
-	return (sum);
-
-		
 }
