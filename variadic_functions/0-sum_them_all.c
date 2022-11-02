58fcb27199cd0234e4fe6484fef0e2891fe9d79a
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-02 08:16:51 -0700

    ex 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,11 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 
 	va_start(num, n);
-	
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(num, int);
 
 	va_end(num);
 
-	return (sum);	
+	return (sum);
 }
