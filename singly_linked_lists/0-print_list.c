9f9760c67a7640996ce360119f59cdf03f747718
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-15 01:01:43 -0800

    ex 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdarg.h>
-#include <stdio>
+#include <stdio.h>
 
 /**
  * print_list - function that prints all the elements of a list_t list.
@@ -17,11 +17,11 @@ unsigned int nodes = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-			nodes++;
-			h = h->next;
+		nodes++;
+		h = h->next;
 	}
 	return (nodes);
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,14 +1,13 @@
 #ifndef LISTS_H
 #define LIST_H
 #include <stdarg.h>
+#include <stddef.h>
 /**
  * putchar - writes the character c to stdout.
  * @char: string argument.
  * Return: On success 1.
  */
 int _putchar(char);
-
-size_t print_list(const list_t *h);
 
 /**
  * struct list_s - singly linked list
@@ -24,5 +23,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
