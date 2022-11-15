69444d5e41d1f9b0ca6f913e49940a5e3e50b314
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-15 01:52:08 -0800

    ex 1

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * putchar - writes the character c to stdout.
  * @char: string argument.
@@ -25,5 +25,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
