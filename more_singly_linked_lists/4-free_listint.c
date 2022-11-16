301f7fee47cdcb94b3a7ccc6868205bc1f03842f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-16 02:23:03 -0800

    ex 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * free_listint - function that frees a listint_t list.
+ * listint_t list.
+ * @head: A pointer to the head of the list_t list.
+ * Return: nothing.
+ */
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next
+		free(temp->n);
+		free(temp);
+	}
+}
