750c140dfb38b59d489de43b928cf5d6aa6105a9
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-16 02:10:46 -0800

    ex 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -28,7 +28,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		temp = head;
 		while (temp->next != NULL)
 			temp = temp->next;
-		last->next = new;
+		temp->next = new;
 	}
 	return (new);
 }
