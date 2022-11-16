5c22a720200452acfad29591abb5a2ec0c762582
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-16 06:53:51 -0800

    ex 5

---
+++ more_singly_linked_lists/4-free_listint_other_sol.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * free_listint - function that frees a listint_t list.
+ * listint_t list.
+ * @head: A pointer to the head of the list_t list.
+ * Return: nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(head);
+}
---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * listint_t list.
+ * @head: A pointer to the head of the list_t list.
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = (*head)->next;
+		free(head);
+		*head = temp;
+	}
+}
