25577497e8b6bb8e249eb4c9964eb4406b882662
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-17 02:56:15 -0800

    ex 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 	new->next = temp->next;
-	temp->next = new
+	temp->next = new;
 
 	return (new);
 }
