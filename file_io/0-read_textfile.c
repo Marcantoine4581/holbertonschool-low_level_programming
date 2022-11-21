cee5730765a28fcf46f43fb5cdf2bdc9407f5225
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-21 05:20:09 -0800

    ex 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,14 +14,24 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, r, w;
-	char buffer;
+	char *buffer;
 
-	buffer = malloc(sizeof(letters));
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
 
 	fd = open(filename, O_RDONLY);
-	r = read(fd, buffer, letters)
+	r = read(fd, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
+	if (fd == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
 	close(fd);
 
 	return (w);
