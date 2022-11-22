91b99c49a139254cd557b78a71d5cc934913a1f6
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-22 02:33:13 -0800

    ex 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,10 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = temp;
-		new->prev = NULL;
-		*h = new;
-		return (new);
+		return (add_dnodeint(h, n));
 	}
 	for (node = 0; node < (idx - 1); node++)
 	{
