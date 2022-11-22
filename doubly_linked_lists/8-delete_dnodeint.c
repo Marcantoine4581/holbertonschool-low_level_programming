6732cc841ceeece5b18ad26e4439db2299243cec
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-22 03:32:04 -0800

    ex 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		(*head)->prev = NULL;
+		if (*head)
+			(*head)->prev = NULL;
 		free(delnode);
 		return (1);
 	}
