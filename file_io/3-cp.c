f0b4e4d161e9b7b025ade3b66f0d6e5d018ff984
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-25 01:18:21 -0800

    ex 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,17 +11,46 @@
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	int from, to, r;
+	int from, to, r, w, cfrom, cto;
 	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	buffer = malloc(sizeof(char) * 1024);
 
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	write(to, buffer, r);
+	w = write(to, buffer, r);
+
+	if (from == -1 || r == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		free(buffer);
+		exit(98);
+	}
+
+	if (to == -1 || w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		free(buffer);
+		close(from);
+		close(to);
+		exit(99);
+	}
+
+	cfrom = close(from), cto = close(to);
+	if (cfrom == -1 || cto == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
 
 	free(buffer);
 	close(from);
