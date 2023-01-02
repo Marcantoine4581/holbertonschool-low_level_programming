8e87155bae083eddf407a13c00d230e373123bbc
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-02 07:06:17 -0800

    comments for return added

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,10 +6,15 @@
  * @array: is a pointer to the first element of the array to search in.
  * @size: is the number of elements in array.
  * @value: is the value to search for.
+ * Return: On success, return the first index where value is located,
+ * If value is not present in array or if array is NULL, return -1.
  */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
+	if (array == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
