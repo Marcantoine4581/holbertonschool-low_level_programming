020fecdc83c665cceaffbb34ab38e797fb754d2e
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-02 08:07:22 -0800

    added <= in the while loop

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (left < right)
+	while (left <= right)
 	{
 		printf("Searching in array: ");
 		for (middle = left; middle < right; middle++)
